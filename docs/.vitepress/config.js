9474dbd5b257a867b169ae349b395cc6228045a9
andsr157 <andikasatrio159@gmail.com>
2024-03-13 12:17:13 +0700

    first commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,29 @@
+export default {
+  title: "Kelompok 1",
+  description: "Etika Iptek",
+
+  themeConfig: {
+    sidebar: [
+      {
+        text: "Tugas 9",
+        collapsible: true,
+        items: [{ text: "nama", link: "/tugas9/nama.md" }],
+      },
+      {
+        text: "Tugas 10",
+        collapsible: false,
+        items: [{ text: "nama", link: "/getting-started" }],
+      },
+      {
+        text: "Tugas 11",
+        collapsible: true,
+        items: [{ text: "nama", link: "/getting-started" }],
+      },
+      {
+        text: "Tugas 12",
+        collapsible: true,
+        items: [{ text: "nama", link: "/getting-started" }],
+      },
+    ],
+  },
+}
