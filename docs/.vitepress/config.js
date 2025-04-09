1ac990404d4f04a0b3afe2b9aa468fee84883c92
frontend-samir <frontendsamir@gmail.com>
2025-04-09 10:39:48 +0700

    update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,6 +3,20 @@ export default {
   description: "Survey Api Contract Document",
 
   themeConfig: {
+    sidebar:[
+      {
+        text:'Documentation',
+        items:[
+          { 
+            text: 'CMS', 
+            items:[
+              {text: 'Survey', link: '/survey'}
+            ]
+          },
+          { text: 'Telemarket', link: '/telemarket' },
+        ]
+      }
+    ],
     footer: {
       copyright: "Copyright © 2024",
     },
